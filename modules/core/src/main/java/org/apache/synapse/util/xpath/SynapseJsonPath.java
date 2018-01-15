6753b35091bbb966b3911bfc8f7c300f5e392c05
Milinda <milindape@gmail.com>
2018-01-15 15:19:29 +0530

    Change error to debug jsonpath for nonexisting element (#249)
    Change error log to debug since an InvalidPathException is thrown when jsonPath points to nonexisting json element.
    And quering nonexisting JSON element is valid use-case and jsonPath validation should happen at JsonPath.compile(jsonPathExpression);
    Fix: https://github.com/wso2/product-ei/issues/1718

--- modules/core/src/main/java/org/apache/synapse/util/xpath/SynapseJsonPath.java
+++ modules/core/src/main/java/org/apache/synapse/util/xpath/SynapseJsonPath.java
@@ -122,7 +122,10 @@ public class SynapseJsonPath extends SynapsePath {
         } catch (IOException e) {
             handleException("Error evaluating JSON Path <" + jsonPath.getPath() + ">", e);
         } catch (Exception e) { // catch invalid json paths that do not match with the existing JSON payload.
-            log.error("#stringValueOf. Error evaluating JSON Path <" + jsonPath.getPath() + ">. Returning empty result. Error>>> " + e.getLocalizedMessage());
+            if (log.isDebugEnabled()) {
+                log.debug("#stringValueOf. Error evaluating JSON Path <" + jsonPath.getPath()
+                        + ">. Returning empty result. Error>>> " + e.getLocalizedMessage());
+            }
             return "";
         }
         if (log.isDebugEnabled()) {
